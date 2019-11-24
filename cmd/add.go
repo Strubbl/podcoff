96d4ab18e17aa83e836f543462e03c05a72dddd7
Sven Fischer <git-dev@linux4tw.de>
2019-11-24 15:08:40 +0100

    polished add cmd

--- cmd/add.go
+++ cmd/add.go
@@ -9,23 +9,21 @@ import (
 var AddName string
 var AddFeedURL string
 
-const numOfArgs = 2
+const numOfAddArgsAllowed = 2
 
 // addCmd represents the add command
 var addCmd = &cobra.Command{
 	Use:   "add [name] [feedURL]",
-	Short: "Add a podcast",
+	Short: "Add podcast",
 	Long: `Add a new podcast to the database
 
 You need to give a name and the url in that order.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called with the following args:", args)
-		fmt.Println("num of args:", len(args))
-		if len(args) == numOfArgs {
+		if len(args) == numOfAddArgsAllowed {
 			AddName = args[0]
 			AddFeedURL = args[1]
 		} else {
-			fmt.Println("invalid number of arguments, got", len(args), "but expected", numOfArgs)
+			fmt.Println("invalid number of arguments, got", len(args), "but expected", numOfAddArgsAllowed)
 			cmd.Help()
 		}
 	},
