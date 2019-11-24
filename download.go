ca80bf7b2475b09c29c776938b185e50f2818307
Sven Fischer <git-dev@linux4tw.de>
2019-11-24 17:25:39 +0100

    add exec command

--- download.go
+++ download.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"podcoff/cmd"
 	"strings"
+	"sync"
 	"syscall"
 )
 
@@ -15,14 +18,17 @@ func downloadItems(p Podcast, c Configuration) error {
 	}
 	for i := 0; i < len(pis); i++ {
 		if pis[i].Status == FRESH {
-			downloadPodcastItem(pis[i].Link, p, c)
+			err = downloadPodcastItem(pis[i].Link, p, c)
+			if err != nil {
+				fmt.Println("Error downloading in podcast", p.Name, "the item", pis[i].Link, ":", err)
+			}
 		}
 	}
 	return nil
 }
 
 func downloadPodcastItem(url string, p Podcast, c Configuration) error {
-	var binary string
+	var command string
 	var downloadHandler string
 	var args []string
 
@@ -36,20 +42,41 @@ func downloadPodcastItem(url string, p Podcast, c Configuration) error {
 	downloadHandlerParams := strings.Fields(downloadHandler)
 	if len(downloadHandlerParams) > 1 {
 		args = append(downloadHandlerParams[1:], url)
-		binary = downloadHandlerParams[0]
+		command = downloadHandlerParams[0]
 	} else {
 		args = append(args, url)
-		binary = downloadHandler
+		command = downloadHandler
 	}
-	if cmd.Debug {
-		log.Println("downloadPodcastItem: binary is", binary)
-		log.Println("downloadPodcastItem: downloadHandlerParams are", downloadHandlerParams)
-		log.Println("downloadPodcastItem: args are", args)
+	binary, err := exec.LookPath(command)
+	if err != nil {
+		return err
 	}
 	env := os.Environ()
-	err := syscall.Exec(binary, args, env)
+	if cmd.Debug {
+		log.Println("downloadPodcastItem: binary is", binary)
+		log.Println("downloadPodcastItem: args are", args)
+	}
+	err = syscall.Exec(binary, args, env)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+// https://stackoverflow.com/a/20438245/709697
+func exe_cmd(cmd string, wg *sync.WaitGroup) {
+	if cmd.Debug {
+		fmt.Println("command is ", cmd)
+	}
+	// splitting head => g++ parts => rest of the command
+	parts := strings.Fields(cmd)
+	head := parts[0]
+	parts = parts[1:len(parts)]
+
+	out, err := exec.Command(head, parts...).Output()
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	fmt.Printf("%s", out)
+	wg.Done() // Need to signal to waitgroup that this goroutine is done
 }
